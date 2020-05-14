eded20e84e8afd9283adca3aa332c0eb8bef0b6e
wowowowowowowow <65040067+wowowowowowowow@users.noreply.github.com>
2020-05-14 11:21:02 -0500

    experimenting
    wow

--- Fps-Yeet/Assets/Scripts/ControlScript.cs
+++ Fps-Yeet/Assets/Scripts/ControlScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ControlScript : MonoBehaviour
 {
+    public CharacterController controller;
+
     // Start is called before the first frame update
     void Start()
     {
